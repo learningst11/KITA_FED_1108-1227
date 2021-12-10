d96a4e1dda729519ae243a54c75b1f0f6ebf7f80
learningst11 <learningst11@gmail.com>
2021-12-10 18:26:04 +0900

    ss

--- 001.HTML학습/008.VOGUE_PJ/002.site/js/common.js
+++ 001.HTML학습/008.VOGUE_PJ/002.site/js/common.js
@@ -1,13 +1,37 @@
 // 공통기능구현 JS - common.js
 
-
+///////// 윈도우 스크롤시 이벤트 함수 ////////////
 window.addEventListener("scroll",()=>{
-    let scTop =this.scrollY;
-    console.log("스클로중~~", scTop);
-    
-});
 
+    // 스크롤 위치값
+    let scTop = this.scrollY;
+    // scrollY - 세로축 스크롤 위치값 리턴
+    // this는 화살표함수에서 window를 나타냄
 
+    /******************************************** 
+        윈도우 세로 스크롤 위치값을 가져오는 방법
+        1. this.scrollY
+        2. window.scrollY
+        3. document.scrollingElement.scrollTop
+        4. document.documentElement.scrollTop
+        5. document.querySelector("html").scrollTop
+    ********************************************/
+
+    // console.log("스크롤중~~~",scTop);
+
+    // 상단영역
+    let top = document.querySelector("#top");
+
+    /////// 스크롤위치값이 100이상일때 /////
+    if(scTop>=100){ // 100px이상
+        top.classList.add("on");
+    } /////////// if /////////////
+    else{ // 100px 미만
+        top.classList.remove("on");
+    } ///////// else //////////////
+
+}); ////////////// scroll 이벤트함수 ////////////
+////////////////////////////////////////////////
 
 //////////// 로딩구역 /////////////////////////
 window.addEventListener("DOMContentLoaded",
@@ -28,6 +52,8 @@ function(){
         var tg = document.querySelector(".mowrap");
         // 내부박스
         var mobx = document.querySelector("#mobx");
+        // 상단영역 최상위박스
+        var top = document.querySelector("#top");
 
         // 3. 변경내용: 
         // (1) 대상의 높이값이 0이면 
@@ -45,10 +71,25 @@ function(){
             // console.log("내부박스높이:",inh);
             tg.style.height = inh + "px";
             tg.style.transition = ".4s ease-out";
-        }
+        } //////// if ////////////////
         else{ // 높이값이 0이 아니면
             tg.style.height = "0";
-        }
+        } ///////// else ////////////
+
+        /// 추가변경: #top에 클래스 .mob 추가하여 
+        // 높이값 보이는 화면 + y축방향 스크롤
+        top.classList.toggle("mob");
+        // classList 객체 : 클래스를 넣거나 빼는 기능객체
+        // 아래 3가지 메서드를 잘알아두자!
+        // (1) add(클래스명) - 클래스넣기
+        // (2) remove(클래스명) - 클래스빼기
+        // (3) toggle(클래스명) - 클래스넣기/빼기
+
+
+        // a요소는 기본이동 특성이 있어서
+        // 이것을 무효화 해주는 코드가 return false임!
+        return false;
+        // return키워드는 맨끝에 할것! 주의!
 
     }; ///////////// click /////////////////
 
@@ -61,7 +102,21 @@ function(){
         // 1. 함수호출확인!
         console.log("검색버튼클릭!");
 
-        
+        // 2. 대상선정: .mos
+        let mos = document.querySelector(".mos");
+        // 대상의 높이값
+        let mosh = mos.offsetHeight;
+
+        // 3. 높이값이 0이면 보이게 아니면 숨기기
+        if(mosh===0){
+            mos.style.height = "50px";
+            mos.style.transition = "height .3s ease-out";
+        } //////// if ////////////
+        else {
+            mos.style.height = "0";
+        } //////// else //////////
+
+
 
         // a요소는 기본이동 특성이 있어서
         // 이것을 무효화 해주는 코드가 return false임!
@@ -70,9 +125,6 @@ function(){
 
     }; /////////// click ////////////////////
     /////////////////////////////////////////
-
-
-
 
 
 }); ////////////// 로딩구역 //////////////////
