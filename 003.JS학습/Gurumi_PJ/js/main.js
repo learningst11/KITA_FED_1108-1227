72f93f21b60be6d2c3a2792e986f7aea32460332
learningst11 <learningst11@gmail.com>
2021-12-15 18:27:46 +0900

    ss

--- 003.JS학습/Gurumi_PJ/js/main.js
+++ 003.JS학습/Gurumi_PJ/js/main.js
@@ -8,11 +8,29 @@ window.addEventListener("DOMContentLoaded", () => {
     // 갤러리박스
     let gbx = document.querySelector(".gbx");
 
+    // 광클금지변수
+    let prot = 0; // 0-허용, 1-불허용
+
     /***************************************** 
         함수명: goSlide
         기능: 슬라이드 순번을 변경하여 이동함
     *****************************************/
-    const goSlide = (dir) => { // dir - 방향(1-오른쪽,0-왼쪽)
+    const goSlide = dir => { // dir - 방향(1-오른쪽,0-왼쪽)
+
+        // 0. 광클금지 /////////////
+        if (prot) return; //돌아가!
+        prot = 1; //잠금!
+        setTimeout(() => prot = 0, 400);
+        ///////////////////////////
+
+        // 0.4초후 prot변수를 처음값으로 변경하여 잠금!
+
+        // 광클금지의 원리는 전역변수를 지정하고
+        // 이값에 따라 return을 주고 바로 변경하여 잠그고
+        // 일정시간뒤에 다시 처음 값으로 풀어준다!
+
+        // return은 함수를 빠져나감!
+        // 참고) break는 제어문을 빠져나감!
 
         // 1. 함수호출확인
         // console.log("바꿔라!",dir);
@@ -23,10 +41,17 @@ window.addEventListener("DOMContentLoaded", () => {
         let gimg = gbx.querySelectorAll("img");
 
         // 3. 변경내용
+
         // 3-1. 오른쪽버튼일 경우 : 맨앞 이미지 맨뒤로!
-        if(dir) gbx.appendChild(gimg[0]);
+        if (dir) gbx.appendChild(gimg[0]);
+        // appendChild(처음요소) -> 처음요소 맨뒤로 이동
+
         // 3-2. 왼쪽버튼일 경우 : 맨뒤 이미지 맨앞으로!
-        else gbx.insertBefore(gimg[gimg.length-1],gimg[0]);
+        else gbx.insertBefore(gimg[gimg.length - 1], gimg[0]);
+        // insertBefore(끝요소,처음요소) 
+        // -> 끝요소를 처음요소 앞으로 이동
+        // gimg[개수-1] -> 끝요소번호
+
 
     }; //////////// goSlide 함수 //////////////
     ///////////////////////////////////////////
@@ -37,12 +62,59 @@ window.addEventListener("DOMContentLoaded", () => {
     //  console.log("버튼개수:",abtn.length);
 
     // 오른쪽버튼 클릭시
-    abtn[1].onclick = () => goSlide(1);
+    abtn[1].onclick = () => {
+        goSlide(1); //슬라이드함수
+        clearAuto(); //인터발지우기
+    }; //////// click ////////
 
     // 왼쪽버튼 클릭시
-    abtn[0].onclick = () => goSlide(0);
+    abtn[0].onclick = () => {
+        goSlide(0); //슬라이드함수
+        clearAuto(); //인터발지우기
+    }; //////// click ////////
 
-    
+    /////////////////////////////////////
+    // 자동 넘기기 설정 -> 인터발 함수사용!
+    /////////////////////////////////////
+
+    // 인터발용변수
+    let autoI;
+
+    // 타임아웃용변수
+    let autoT;
+
+    /******************************** 
+        함수명: autoSlide
+        기능: 슬라이드함수 인터발호출
+    ********************************/
+    const autoSlide = () =>
+        autoI = setInterval(() => goSlide(1), 2000);
+    ////////// autoSlide 함수 ///////////
+
+    // setInterval 을 사용시 함수만 호출할 경우
+    // 함수명만 써도 되지만 함수에 전달값을 써야할 경우
+    // 익명함수 형식으로 써야한다! ()=>goSlide(1)
+
+    // 인터발호출함수 최초호출
+    autoSlide();
+
+
+    /************************************** 
+        함수명: clearAuto
+        기능: 인터발함수 지우고 타임아웃셋팅
+    **************************************/
+    const clearAuto = () => {
+        console.log("지움!");
+        // 1. 자동호출지우기
+        clearInterval(autoI);
+        // 2. 타임아웃지우기(실행쓰나미방지!)
+        clearTimeout(autoT);
+        // 3. 자동호출 타임아웃셋팅
+        autoT = setTimeout(autoSlide, 3000);
+    }; /////////// clearAuto 함수 ///////////
+
+
+
 
 
 }); ///////////// 로딩구역 //////////////////////
